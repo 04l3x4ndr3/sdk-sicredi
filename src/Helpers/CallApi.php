6de89e7b0f70b184756c9d01946f1e7b6c1aff99
Diego <diegokock90@gmail.com>
2023-11-16 10:13:05 -0300

    fix - ajuste nas opções de headers no modo produção

--- src/Helpers/CallApi.php
+++ src/Helpers/CallApi.php
@@ -8,191 +8,68 @@ use O4l3x4ndr3\SdkSicredi\Configuration;
 
 class CallApi
 {
-    /**
-     * @var Configuration
-     */
     private Configuration $config;
-    /**
-     * @var array|string[]|null
-     */
     private ?array $header;
-    /**
-     * @var array|null
-     */
-    private ?array $credential;
-    /**
-     * @var array
-     */
+    private ?array $keys;
     private array $options;
-    private string $cooperativa = '6789';
-    private string $posto = '03';
-    private string $codigoBeneficiario = '12345';
-    private string $authorization = 'Bearer ';
 
-    /**
-     * @param Configuration $config
-     */
-    public function __construct(Configuration $config)
+    public function __construct(?Configuration $config = NULL)
     {
         $this->config = $config ?? new Configuration();
-        $this->credential = $this->config->getCredentials();
-        $this->header = array_merge([
-            'User-Agent' => 'SDKSicredi/1.0',
-            'Content-Type' => 'application/json'
-        ], $this->config->getHttpHeader());
+        $this->keys = $this->config->getKeys();
     }
 
     /**
-     * @return Configuration
+     * @throws GuzzleException
      */
-    public function getConfig(): Configuration
+    public function accessToken(): object
     {
-        return $this->config;
-    }
-
-    /**
-     * @param Configuration $config
-     * @return void
-     */
-    public function setConfig(Configuration $config): void
-    {
-        $this->config = $config;
-    }
-
-    public function getHeader(): ?array
-    {
-        return $this->header;
-    }
-
-    public function setHeader(?array $header): CallApi
-    {
-        $this->header = $header;
-        return $this;
-    }
-
-    public function getCredential(): ?array
-    {
-        return $this->credential;
-    }
-
-    public function setCredential(?array $credential): CallApi
-    {
-        $this->credential = $credential;
-        return $this;
-    }
-
-    public function getOptions(): array
-    {
-        return $this->options;
-    }
-
-    public function setOptions(array $options): CallApi
-    {
-        $this->options = $options;
-        return $this;
-    }
-
-    public function getCooperativa(): string
-    {
-        return $this->cooperativa;
-    }
-
-    public function setCooperativa(string $cooperativa): CallApi
-    {
-        $this->cooperativa = $cooperativa;
-        return $this;
-    }
-
-    public function getPosto(): string
-    {
-        return $this->posto;
-    }
-
-    public function setPosto(string $posto): CallApi
-    {
-        $this->posto = $posto;
-        return $this;
-    }
-
-    public function getCodigoBeneficiario(): string
-    {
-        return $this->codigoBeneficiario;
-    }
-
-    public function setCodigoBeneficiario(string $codigoBeneficiario): CallApi
-    {
-        $this->codigoBeneficiario = $codigoBeneficiario;
-        return $this;
-    }
-
-    public function getAuthorization(): string
-    {
-        return $this->authorization;
-    }
-
-    public function setAuthorization(string $authorization): CallApi
-    {
-        $auth = $this->getAuthorization() . $authorization;
-        $this->authorization = $auth;
-        return $this;
+        $client = new Client();
+        $options = [
+            'headers' => [
+                'x-api-key' => $this->keys['x-api-key'],
+                'context' => 'COBRANCA',
+                'Content-Type' => 'application/x-www-form-urlencoded',
+            ],
+            'form_params' => $this->config->getCredentials()
+        ];
+        $res = $client->request('POST', $this->config->getUrl() . '/sb/auth/openapi/token', $options);
+        return json_decode($res->getBody()->getContents());
     }
 
     /**
      * @param string $endpoint
      * @param array|null $body
      * @param string|null $method
-     * @return array|object
      * @throws GuzzleException
      */
-    public function call(string $endpoint, ?array $header = NULL, ?array $body = NULL, ?string $method = 'POST'): array|object
+    public function call(string $endpoint, ?array $body = NULL, ?string $method = 'POST')
     {
-        $x_api_key = $this->credential['x-api-key'];
+        $token = $this->accessToken();
 
         $client = new Client();
-        $options = [
-            'headers' => array_filter([
-                'http_errors' => false,
-                'Content-Type' => 'application-json',
-                'x-api-key' => $x_api_key,
-                'Authorization' => $this->getAuthorization(),
-                $header
-            ], function ($v) {
-                return !empty($v);
-            }),
-            'form_params' => array_filter([$body], function ($v) {
-                return !empty($v);
-            })
-        ];
-
-        $this->options = $options;
+        if (isset($body)) {
+            $options = array_filter([
+                'headers' => [
+                    'x-api-key' => $this->keys['x-api-key'],
+                    'Authorization' => "{$token->token_type} {$token->access_token}",
+                    'Content-Type' => 'application/json',
+                    'cooperativa' => $this->keys['cooperativa'],
+                    'posto' => $this->keys['posto'],
+                ],
+                'json' => $body
+            ]);
+        } else {
+            $options = array_filter([
+                'headers' => [
+                    'x-api-key' => '65a3ad4e-f73a-4a25-87db-55d3e04b1516',
+                    'Authorization' => "{$token->token_type} {$token->access_token}",
+                ]
+            ]);
+        }
         $res = $client->request($method, "{$this->config->getUrl()}{$endpoint}", $options);
+        $res = json_decode($res->getBody()->getContents());
 
-        $data = $res->getBody()->getContents();
-
-        return json_decode($data);
-    }
-
-    /**
-     * @return void
-     * @throws GuzzleException
-     */
-    public function authentication(): void
-    {
-        $header = [
-            'Content-Type' => 'application/x-www-form-urlencoded',
-            'context' => 'COBRANCA'
-        ];
-        $body = [
-            'username' => $this->credential['username'],
-            'password' => $this->credential['password'],
-            'scope' => 'cobranca',
-            'grant_type' => 'password',
-            'cooperativa' => $this->getCooperativa(),
-            'posto' => $this->getPosto(),
-            'codigoBeneficiario' => $this->getCodigoBeneficiario(),
-        ];
-        $call = $this->call("/sb/auth/openapi/#token", $header, $body);
-
-        $this->setAuthorization($call->id_token);
+        return $res;
     }
 }
